89a35de85b35bf0154b8cda7d6a34fc2ad93f1b7
Attila Reterics <reterics.attila@gmail.com>
2023-12-14 19:49:14 +0100

    Monteuren booking logic

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require(__DIR__ . '/lib/lang.php');
 require(__DIR__ . '/lib/sql.php');
+$configs = include(__DIR__ . '/config.php');
+
 $lng = new Lang();
 $sqlMGR = new SQLMgr();
 $aufid = false;
@@ -18,7 +20,9 @@ $globalCalendarData = array(
     'work_length' => '90',
     'wartdatum' => null,
     'wartzeit' => null,
-    'post' => ''
+    'post' => '',
+    'monteur' => 'default',
+    'monteuren'=> $configs['monteuren']
 );
 if (isset($_POST['aufid'])) {
 
@@ -87,6 +91,8 @@ if ($aufid) {
         $globalCalendarData['date_to'] = $data['wartdatumbis'];
         $globalCalendarData['wartzeit'] = $data['wartzeit'];
         $globalCalendarData['wartdatum'] = $data['wartdatum'];
+        $monteur = $string = str_replace(' ', '', $data['monteur'] ?? 'default');
+        $globalCalendarData['monteur'] = isset($configs['monteuren'][$monteur]) ? $monteur : 'default';
         $globalCalendarData['loaded'] = true;
     }
 }
